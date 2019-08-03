5e623336cc1d2203e1e90b57f7182350f86f1807
Laplayax <karlkoeppen@hotmail.de>
2019-08-03 21:37:59 +0200

    Fixed Images of Artbox

--- src/components/artbox/index.js
+++ src/components/artbox/index.js
@@ -1,6 +1,6 @@
-import React from "react"
-import { graphql, navigate, StaticQuery } from "gatsby"
-import "./artbox.css"
+import React from 'react';
+import { graphql, navigate, StaticQuery } from 'gatsby';
+import './artbox.css';
 
 export default () => (
   <StaticQuery
@@ -31,17 +31,28 @@ export default () => (
       <div className="artbox_box">
         <div className="artbox_container">
           {data.allContentfulArticle.edges.map(edge => (
-            <div
-              key={edge.node.id}
-              className="artbox_item"
-              style={{
-                backgroundImage: `url(${edge.node.featuredImage.fluid.src})`,
-              }}
-              onClick={() => navigate(`/articles/${edge.node.slug}`)}
-            ></div>
+            <div className="artbox-cut">
+              <img
+                key={edge.node.id}
+                className="artbox_item"
+                src={edge.node.featuredImage.fluid.src}
+                onClick={() => navigate(`/articles/${edge.node.slug}`)}
+              ></img>
+            </div>
           ))}
         </div>
       </div>
     )}
   />
-)
+);
+
+{
+  /* <div
+key={edge.node.id}
+className="artbox_item"
+style={{
+  backgroundImage: `url(${edge.node.featuredImage.fluid.src})`
+}}
+onClick={() => navigate(`/articles/${edge.node.slug}`)}
+></div> */
+}
