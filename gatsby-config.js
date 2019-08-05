86bf68f4edf2c0258f57ad93ecb740ae06f38a2c
Laplayax <karlkoeppen@hotmail.de>
2019-08-05 19:18:33 +0200

    Gatsby plugin for sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,29 +1,30 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+});
 
 module.exports = {
   siteMetadata: {
     title: `Bird Esports`,
     description: `Bird Esports is a new League of Legends organization focused on the upbringing of ambitious players towards the amateur scene.`,
-    author: `@gatsbyjs`,
+    author: `@gatsbyjs`
   },
   plugins: [
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-remark`,
     {
-      resolve: "gatsby-source-contentful",
+      resolve: 'gatsby-source-contentful',
       options: {
-        spaceId: "pbadwl9vfcqs",
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-      },
+        spaceId: 'pbadwl9vfcqs',
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -36,11 +37,11 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
-      },
-    },
+        icon: `src/images/favicon.png` // This path is relative to the root of the site.
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
-}
+  ]
+};
